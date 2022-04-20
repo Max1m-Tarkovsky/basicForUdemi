5957c5199a9d40010a16849fa432ea055bafe73c
Max1m <tarkovsky061295@gmail.com>
2022-04-20 18:26:32 +0300

    some task

--- SomeTask/taskCycles**.js
+++ SomeTask/taskCycles**.js
@@ -2,7 +2,7 @@ const lines = 5;
 let result = '';
 
 for (let i = 0; i <= lines; i++) {
-    for (let j = 0; j < lines - i; j++) {
+    for (let j = 0; j < lines-i; j++) {
         result += " ";
     }
     for (let j = 0; j < 2 * i + 1 ; j++) {
