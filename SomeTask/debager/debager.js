69f4bcb9732728790a86ddb6d7e8134af17c178f
Max1m <tarkovsky061295@gmail.com>
2022-05-06 12:32:53 +0300

    debager (check not my code with debager and refactor and change someone else's code)

---
+++ SomeTask/debager/debager.js
@@ -0,0 +1,57 @@
+const restorantData = {
+    menu: [
+        {
+            name: 'Salad Caesar',
+            price: '14$'
+        },
+        {
+            name: 'Pizza Diavola',
+            price: '9$'
+        },
+        {
+            name: 'Beefsteak',
+            price: '17$'
+        },
+        {
+            name: 'Napoleon',
+            price: '7$'
+        }
+    ],
+    waitors: [
+        {name: 'Alice', age: 22}, {name: 'John', age: 24}
+    ],
+    averageLunchPrice: '20$',
+    openNow: true
+};
+
+function isOpen(prop) {
+    let answer = '';
+    prop ? answer = 'Открыто' : answer = 'Закрыто';
+
+    return answer;
+}
+
+console.log(isOpen(restorantData.openNow));
+
+function isAverageLunchPriceTrue(fDish, sDish, average) {
+
+    if (+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1)) < (+average.slice(0, -1))) {
+        return 'Цена ниже средней';
+    } else {
+        return 'Цена выше средней';
+    }
+}
+
+console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
+
+function transferWaitors(data) {
+
+    let clone = Object.assign({}, data);
+    clone.waitors = [{name: 'Mike', age: 32}];
+
+    return clone;
+}
+
+
+console.log((transferWaitors(restorantData)));
+console.log(restorantData);
